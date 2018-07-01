e4a48d42260d70a6e146e2cba331bac95f443d9a
Blessing Krofegha <bkrofegha@gmail.com>
2018-07-01 21:22:52 +0100

    updated server.js

--- server.js
+++ server.js
@@ -9,12 +9,9 @@ app.get('/', function(req, res) {
 });
 
 //set static files
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname));
 
 //start server
 app.listen(8000, function() {
   console.log('Example app listening on port 8000!');
 });
-
-
-
